0585c25284346dd3acb7abf2b66ee3f3196e867e
Nikita Skorodumov <mr-zerx@yandex.ru>
2020-08-20 13:51:11 +0500

    Homework 2

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
-console.log('Hello World!!!');
+const firstRow = 'мама мыла раму';
+const secondRow = 'собака друг человека';
+
+alert(`Даны две строки: \'${firstRow}\' и \'${secondRow}\'. Сейчас скажу в которой больше букв а.`);
+
+
+function countSymbol(row) {
+    let counter = 0;
+    for (let i = 0; i < row.length; i++) {
+        if(row.charAt(i) === 'а') {
+            counter++
+        }
+    }
+    return counter;
+}
+
+function getRow(firstRow, secondRow) {
+    let firstCounter = countSymbol(firstRow);
+    let secondCounter = countSymbol(secondRow);
+
+    return (firstCounter > secondCounter) ? firstRow : secondRow;
+}
+
+console.log(getRow(firstRow, secondRow)); // мама мыла раму
+
+alert('Больше всего букв а в строке : \'' + getRow(firstRow, secondRow) + '\'')
